b1fd6f450cb76e2b28ec405670f399372aea0ca2
Fabio Benjovengo <fabio.benjovengo@toradex.com>
2025-11-03 13:22:08 -0300

    add(patches): add a patch script

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello, Yocto from Git!\n");
-    return 0;
-}
---
+++ src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, world from Yocto via autotools!!\n");
+    return 0;
+}
